2c218a6bd0e8612031555f4a86a08ec15c8a3349
Michael Sumner <mdsumner@gmail.com>
2022-03-14 11:12:02 +1100

    Initial commit

---
+++ fix-oisst.R
@@ -0,0 +1,28 @@
+u <- "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/v2.1/access/avhrr/202202/oisst-avhrr-v02r01.20220218.nc"
+f0 <- basename(u)
+if (!file.exists(f0)) curl::curl_download(u, f0)
+
+f <- "file.nc"
+file.copy(f0, f)
+library(RNetCDF)
+nc <- open.nc(f, write = TRUE)
+crsvar <- "crs"
+var <- var.def.nc(nc, crsvar, "NC_INT", NA)
+var.put.nc(nc, crsvar, 1L)
+att.put.nc(nc, crsvar, "comment", "NC_STRING",
+           "This is a container variable that describes the grid_mapping used by the data in this file. This variable does not contain any data; only information about the geographic coordinate system."
+)
+att.put.nc(nc, crsvar, "grid_mapping_name", "NC_STRING", "latitude_longitude")
+att.put.nc(nc, crsvar, "inverse_flattening", "NC_FLOAT", 298.257)
+att.put.nc(nc, crsvar, "semi_major_axis", "NC_FLOAT", 6378136.3)
+
+library(dplyr)
+vars <- ncmeta::nc_vars(nc) %>% dplyr::filter(ndims == 4) %>% pull(name)
+for (varname in vars[-1]) {
+  #{varname}:coordinates = "longitude latitude" ;
+  #{varname}:grid_mapping = "{crsvar}" ;
+  att.put.nc(nc, varname, "coordinates", "NC_STRING", "longitude latitude")
+  att.put.nc(nc, varname, "grid_mapping", "NC_STRING",  crsvar)
+}
+close.nc(nc)
+
